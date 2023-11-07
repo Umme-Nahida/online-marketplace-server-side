a66061c7d9dca67235fa223dcbb64e0d3bcf077e
emamuddin10 <imamu854@gmail.com>
2023-11-08 01:41:41 +0600

    create a api for get the all jobs data from the mongodb

--- index.js
+++ index.js
@@ -96,7 +96,7 @@ async function run() {
      const result = await bidProjectCollection.updateOne(query,updateBid)
      res.send(result)
   })
-  
+
   app.patch('/updateBidCompletStatus/:id',async(req,res)=>{
      const id = req.params.id;
      const query = {_id: new ObjectId(id)}
@@ -117,7 +117,6 @@ async function run() {
       res.send(result)
     })
 
-     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
