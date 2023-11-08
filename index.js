1836effaef020e42bf45ca86c1ca838531ec367c
emamuddin10 <imamu854@gmail.com>
2023-11-08 11:54:03 +0600

    store the all bid jobs data on the mongodb bidProjectCollection

--- index.js
+++ index.js
@@ -31,6 +31,12 @@ async function run() {
       const jobCollections = database.collection("jobCollection");
       const bidProjectCollection = database.collection("bidProjectCollection");
 
+
+      app.put('/updatePostJob/:id',(req,res)=>{
+         const id = req.params.id;
+         const query = {_id: new ObjectId(id)}
+         const 
+      })
        //  delete the job from my posted job page and jobCollectionDB
        app.delete('/myPostedJobDelet/:id', async (req, res) => {
          const id = req.params.id;
@@ -42,9 +48,9 @@ async function run() {
       app.get('/jobDetails/:id', async (req, res) => {
          const id = req.params.id;
          console.log(id)
-         const query = { _id: new ObjectId(id) }
-         const result = await jobCollections.findOne(query)
-         res.send(result)
+         // const query = { _id: new ObjectId(id) }
+         // const result = await jobCollections.findOne(query)
+         // res.send(result)
       })
       //  get the allJobs from mongoDB jobCollections
       app.get('/alljobs', async (req, res) => {
