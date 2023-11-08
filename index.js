eb940a29a5835519a8d7d331b716fbb4820e5e11
emamuddin10 <imamu854@gmail.com>
2023-11-08 11:41:51 +0600

    get the specific job from the job Collections

--- index.js
+++ index.js
@@ -16,113 +16,122 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
+   serverApi: {
+      version: ServerApiVersion.v1,
+      strict: true,
+      deprecationErrors: true,
+   }
 });
 
 
 async function run() {
-  try {
-    await client.connect();
-    const database = client.db("jobsDB");
-    const jobCollections =database.collection("jobCollection");
-    const bidProjectCollection =database.collection("bidProjectCollection");
-   
-    app.get('/alljobs',async(req,res)=>{
-         const result = await jobCollections.find({}).toArray()
-         res.send(result)
-    })
-    app.post('/allJobs', async (req, res) => {
-      const jobs = req.body;
-      console.log(jobs)
-      const result = await jobCollections.insertOne(jobs)
-      res.send(result)
-    })
+   try {
+      await client.connect();
+      const database = client.db("jobsDB");
+      const jobCollections = database.collection("jobCollection");
+      const bidProjectCollection = database.collection("bidProjectCollection");
 
-    // creat api for jobDetails 
-    app.get('/jobDetails/:id',async(req,res)=>{
+       //  delete the job from my posted job page and jobCollectionDB
+       app.delete('/myPostedJobDelet/:id', async (req, res) => {
+         const id = req.params.id;
+         const query = { _id: new ObjectId(id) }
+         const result = await jobCollections.deleteOne(query)
+         res.send(result)
+      })
+      // get specific job from jobCollectionsDB
+      app.get('/jobDetails/:id', async (req, res) => {
          const id = req.params.id;
          console.log(id)
-         const query = {_id: new ObjectId(id)}
+         const query = { _id: new ObjectId(id) }
          const result = await jobCollections.findOne(query)
          res.send(result)
-    }) 
-    
-  //  display all my bids on the My bids page
-   app.get('/displayMyBids',async(req,res)=>{
-      console.log(req.query.email)
-      let query = {};
-      if(req.query.email){
-         query = {email: req.query.eamil}
-      }
-      const result = await bidProjectCollection.find(query).toArray()
-      res.send(result)
-      
-   })
-   
-  // delet the bid jobs project 
-  app.delete('/bidJobs/:id',async(req,res)=>{
-     const id = req.params.id;
-     console.log(id)
-     const query = {_id: new ObjectId(id)};
-     const result = await bidProjectCollection.deleteOne(query)
-     res.send(result)
-  })
+      })
+      //  get the allJobs from mongoDB jobCollections
+      app.get('/alljobs', async (req, res) => {
+         const result = await jobCollections.find({}).toArray()
+         res.send(result)
+      })
 
-  app.patch('/updateBidProgressStatus/:id',async(req,res)=>{
-     const id = req.params.id;
-     console.log(id)
-     const query = {_id: new ObjectId(id)}
-     const updateBid = {
-        $set: {
-           status: "progress"
-        }
-     }
-     const result = await bidProjectCollection.updateOne(query,updateBid)
-     res.send(result)
-  })
+      // insert the all jobs on the mongoDB jobCollection 
+      app.post('/allJobs', async (req, res) => {
+         const jobs = req.body;
+         console.log(jobs)
+         const result = await jobCollections.insertOne(jobs)
+         res.send(result)
+      })
 
-  app.patch('/updateBidRejectStatus/:id',async(req,res)=>{
-     const id = req.params.id;
-     const query = {_id: new ObjectId(id)}
-     const updateBid = {
-        $set: {
-           status: 'cancelled'
-        }
-     }
-     const result = await bidProjectCollection.updateOne(query,updateBid)
-     res.send(result)
-  })
+      //  display all my bids on the My bids page
+      app.get('/displayMyBids', async (req, res) => {
+         console.log(req.query.email)
+         let query = {};
+         if (req.query.email) {
+            query = { email: req.query.eamil }
+         }
+         const result = await bidProjectCollection.find(query).toArray()
+         res.send(result)
 
-  app.patch('/updateBidCompletStatus/:id',async(req,res)=>{
-     const id = req.params.id;
-     const query = {_id: new ObjectId(id)}
-     const updateBid = {
-        $set: {
-           status: 'completed'
-        }
-     }
-     const result = await bidProjectCollection.updateOne(query,updateBid)
-     res.send(result)
-  })
+      })
 
-    // create api for add bid job  on the database 
-    app.post('/storeBidJobs',async(req,res)=>{
-      const bidJobs = req.body;
-      console.log(bidJobs)
-      const result = await bidProjectCollection.insertOne(bidJobs);
-      res.send(result)
-    })
+      // delet the bid jobs project 
+      app.delete('/bidJobs/:id', async (req, res) => {
+         const id = req.params.id;
+         console.log(id)
+         const query = { _id: new ObjectId(id) };
+         const result = await bidProjectCollection.deleteOne(query)
+         res.send(result)
+      })
 
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    // await client.close();
-  }
+      app.patch('/updateBidProgressStatus/:id', async (req, res) => {
+         const id = req.params.id;
+         console.log(id)
+         const query = { _id: new ObjectId(id) }
+         const updateBid = {
+            $set: {
+               status: "progress"
+            }
+         }
+         const result = await bidProjectCollection.updateOne(query, updateBid)
+         res.send(result)
+      })
+
+      app.patch('/updateBidRejectStatus/:id', async (req, res) => {
+         const id = req.params.id;
+         const query = { _id: new ObjectId(id) }
+         const updateBid = {
+            $set: {
+               status: 'cancelled'
+            }
+         }
+         const result = await bidProjectCollection.updateOne(query, updateBid)
+         res.send(result)
+      })
+
+      app.patch('/updateBidCompletStatus/:id', async (req, res) => {
+         const id = req.params.id;
+         const query = { _id: new ObjectId(id) }
+         const updateBid = {
+            $set: {
+               status: 'completed'
+            }
+         }
+         const result = await bidProjectCollection.updateOne(query, updateBid)
+         res.send(result)
+      })
+
+      // create api for add bid job  on the database 
+      app.post('/storeBidJobs', async (req, res) => {
+         const bidJobs = req.body;
+         console.log(bidJobs)
+         const result = await bidProjectCollection.insertOne(bidJobs);
+         res.send(result)
+      })
+
+      await client.db("admin").command({ ping: 1 });
+      console.log("Pinged your deployment. You successfully connected to MongoDB!");
+   } finally {
+      // Ensures that the client will close when you finish/error
+      // await client.close();
+   }
 }
 run().catch(console.dir);
 
@@ -130,9 +139,9 @@ run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
-  res.send('assignment 11 server is running')
+   res.send('assignment 11 server is running')
 })
 
 app.listen(port, () => {
-  console.log(`assignment 11 server port is ${port}`)
+   console.log(`assignment 11 server port is ${port}`)
 })
